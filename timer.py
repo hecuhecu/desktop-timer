e0268adb8529d63da2741f6090160a684672f3cd
hecuhecu <hecunyuji5@gmail.com>
2021-12-01 00:08:00 +0900

    add ability to disable and enable reset button

--- timer.py
+++ timer.py
@@ -25,37 +25,41 @@ text_min.set("0")
 start_stop_button = StringVar()
 start_stop_button.set("START")
 
-start = True
-stop = False
-check = True
+#動いていない（スタートが無効,ストップが有効）
+start = False
+stop = True
+initial_startup = True
 
-def start():
-    global input_sec,input_min,start,stop,check
-    start = False
-    if stop == True and check == True:
+def tap_start_stop():
+    global input_sec,input_min,start,stop,initial_startup
+
+    if start == False and stop == True and initial_startup == False:
         start = True
+        stop = False
         start_stop_button.set("STOP")
+        button1["state"] = DISABLED
         timer()
-    elif stop == True and check == False:
-        text_sec.set(int(text_sec.get()))
-        text_min.set(int(text_min.get()))
-        check = True
+    elif start == True and stop == False and initial_startup == False:
+        start = False
+        stop = True
         start_stop_button.set("START")
+        button1["state"] = NORMAL
+        timer()
     else:
         start = True
-        stop = True
+        stop = False
+        initial_startup = False
         start_stop_button.set("STOP")
         text_sec.set(int(input_sec.get()))
         text_min.set(int(input_min.get()))
         timer()
-    
+
 def timer():
-    global start,text_sec,text_min,check
+    global start,text_sec,text_min
     if start == True:
         if int(text_sec.get()) == 0 and int(text_min.get()) == 0:
             pass
         else:
-            check = False
             time_min = int(text_min.get())
             time_sec = int(text_sec.get())
             if time_min >= 0:
@@ -74,10 +78,11 @@ def timer():
                 text_min.set(str(time_min))
 
 def stop():
-    global start,stop,check
-    start = True
-    stop = False
-    check = True
+    global start,stop,initial_startup
+    start = False
+    stop = True
+    initial_startup = True
+    button1["state"] = DISABLED
     time_sec = 0
     time_min = 0
     text_sec.set(str(time_sec))
@@ -98,11 +103,11 @@ entry1.grid(row=0,column=3)
 label_sec=Label(root,text="秒")
 label_sec.grid(row=0,column=4)
 
-button=Button(root,textvariable=start_stop_button,command=start)
+button=Button(root,textvariable=start_stop_button,command=tap_start_stop)
 button.grid(row=0,column=5)
 
-button=Button(root,text="RESET",command=stop)
-button.grid(row=0,column=6)
+button1=Button(root,text="RESET",state=DISABLED,command=stop)
+button1.grid(row=0,column=6)
 
 labbel=Label(root,text="タイマー")
 labbel.grid(row=1,column=0,columnspan=1)
